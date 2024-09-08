58be289eb9a2bfdafc3e9df26e9f8e720ce62e3f
AshKemp <kmuraliashwin@gmail.com>
2024-09-08 14:19:43 +0530

    1. create pages 2. use env for firebase api key

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
 const firebaseConfig = {
-  apiKey: "AIzaSyCGCaWFSNUgnFBTAzJmSC_iRx6sst3fdjc",
+  apiKey: FIREBASE_API_KEY,
   authDomain: "react-firebase-auth-2024-a1ac8.firebaseapp.com",
   projectId: "react-firebase-auth-2024-a1ac8",
   storageBucket: "react-firebase-auth-2024-a1ac8.appspot.com",
